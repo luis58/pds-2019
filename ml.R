768714f5802c773c26e31b630373bda26fd5e9a5
LINIOMEX\luis.ochoa <luis.ochoa@linio.com>
2019-09-25 16:20:10 -0500

    completada la función store data

--- ml.R
+++ ml.R
@@ -2,6 +2,6 @@ load_data <- function () {}
 
 clean_data <- function () {} 
 
-store_data <- function () {} 
+store_data <- function () { #esto es un comentario} 
 
 run_ml_pipeline <- function () {} 
